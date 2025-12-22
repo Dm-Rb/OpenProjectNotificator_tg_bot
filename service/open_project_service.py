6a18ad7259936f7519191e6b6214708f543aaa4c
Dm-Rb <dm.rb.418http@gmail.com>
2025-12-22 14:25:32 +0300

    bagfix

--- service/open_project_service.py
+++ service/open_project_service.py
@@ -21,7 +21,8 @@ def customField55(func):
                             if work_package:
                                 if work_package.get('customField55'):
                                     task_info['notify_users'].append({"name": work_package['customField55'], "href": ""})
-                                    task_info['invoice '] = str(work_package['customField51'])
+                                    if work_package.get('customField51'):
+                                        task_info['invoice'] = str(work_package.get('customField51'))
                                     return task_info
                             activity = body_json.get('activity')
                             if activity:
@@ -32,7 +33,8 @@ def customField55(func):
                                         if work_package.get('customField55'):
                                             task_info['notify_users'].append(
                                                 {"name": work_package['customField55'], "href": ""})
-                                            task_info['invoice '] = str(work_package['customField51'])
+                                            if work_package.get('customField51'):
+                                                task_info['invoice'] = str(work_package.get('customField51'))
                                             return task_info
         except Exception as e:
             logger.error("Error in the <customField55> decorator': %s; body_json: %s", str(e), body_json)
